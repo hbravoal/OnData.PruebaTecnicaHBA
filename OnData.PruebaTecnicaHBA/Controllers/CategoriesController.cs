cafc303e3a1d9daad9eb12c3664f3d5156b328fb
UpToken25 <hbravoal@gmail.com>
2018-03-07 16:11:59 -0500

    Create Panel. Look At Feel

---
+++ OnData.PruebaTecnicaHBA/Controllers/CategoriesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnData.PruebaTecnicaHBA.Context;
+using OnData.PruebaTecnicaHBA.Models;
+
+namespace OnData.PruebaTecnicaHBA.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private StoreContext db = new StoreContext();
+
+        // GET: Categories
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = await db.categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name")] category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = await db.categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name")] category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = await db.categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            category category = await db.categories.FindAsync(id);
+            db.categories.Remove(category);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnData.PruebaTecnicaHBA/Controllers/ContainersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnData.PruebaTecnicaHBA.Context;
+using OnData.PruebaTecnicaHBA.Models;
+
+namespace OnData.PruebaTecnicaHBA.Controllers
+{
+    public class ContainersController : Controller
+    {
+        private StoreContext db = new StoreContext();
+
+        // GET: Containers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.containers.ToListAsync());
+        }
+
+        // GET: Containers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            container container = await db.containers.FindAsync(id);
+            if (container == null)
+            {
+                return HttpNotFound();
+            }
+            return View(container);
+        }
+
+        // GET: Containers/Create
+        public ActionResult Create()
+        {
+            
+            
+            
+            
+            return View();
+        }
+
+        // POST: Containers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,type_containers_id,content")] container container)
+        {
+            if (ModelState.IsValid)
+            {
+                db.containers.Add(container);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(container);
+        }
+
+        // GET: Containers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            container container = await db.containers.FindAsync(id);
+            if (container == null)
+            {
+                return HttpNotFound();
+            }
+            return View(container);
+        }
+
+        // POST: Containers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,type_containers_id,content")] container container)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(container).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(container);
+        }
+
+        // GET: Containers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            container container = await db.containers.FindAsync(id);
+            if (container == null)
+            {
+                return HttpNotFound();
+            }
+            return View(container);
+        }
+
+        // POST: Containers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            container container = await db.containers.FindAsync(id);
+            db.containers.Remove(container);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
